538937bdc27c7fc6c9d558f486bee892a5668262
tnugd0 <94063417+tnugd0@users.noreply.github.com>
2023-01-30 23:14:40 +0700

    Update imgkmeans2.py

--- imgkmeans2.py
+++ imgkmeans2.py
@@ -1,16 +1,16 @@
 # done manual
 
-import matplotlib.pyplot as plt # đọc bức ảnh => trả lại ma trận chứa giá trị RGB của từng pixel em nhé   
+import matplotlib.pyplot as plt 
 from sklearn.cluster import KMeans
 import  numpy
 
 img = plt.imread('Original-Image.jpg')
-width = img.shape[0]  # shape trả về số pixel ở chiều ngang, docj, và màu theo không gian(3,2 chiều)
+width = img.shape[0] 
 height = img.shape[1]
-img = img.reshape(width * height,3) # điểu chỉnh lại bức ảnh với 3 chiều và có điểm = dài * rộng (biến nó thành hình ngang)
+img = img.reshape(width * height,3)
 kmeans = KMeans(n_clusters=10).fit(img)
-labels =kmeans.predict(img) # labels là nhãn ứng với từng pixel để có thể đổi màu 
-clusters = kmeans.cluster_centers_ # cluster tọa độ màu RRB là điểm màu phổ biến nhất trong ảnh 
+labels =kmeans.predict(img) 
+clusters = kmeans.cluster_centers_
 
 img2 = numpy.zeros((width,height,3), dtype=numpy.uint8)
 
